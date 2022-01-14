02093c197974be136df58a865c4cb45fea24d7b9
Apurva Hingul <apurvahingul123@gmail.com>
2022-01-14 03:44:50 +0000

    I created a loop which enables the Splash Screen to run for 4.5 secs each time the App is opened.

--- app/src/main/java/com/example/advancehealing/SplashScreen.java
+++ app/src/main/java/com/example/advancehealing/SplashScreen.java
@@ -2,7 +2,10 @@ package com.example.advancehealing;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+
 
 public class SplashScreen extends AppCompatActivity {
 
@@ -10,5 +13,21 @@ public class SplashScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+        Thread thread = new Thread() {
+            @Override
+            public void run() {
+
+                try {
+                    sleep(4500);
+                }catch (Exception e) {
+                    e.printStackTrace();
+                }finally {
+                    startActivity(new Intent(SplashScreen.this, MainActivity.class));
+                    finish();
+                }
+            }
+
+        };
+        thread.start();
     }
 }
